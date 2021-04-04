091d75fbdebd70a2653e03b02e414c99e2cf75be
Elvis Leite da SIlva <38222579+elvis7t@users.noreply.github.com>
2021-04-04 14:50:37 -0300

    Add banco teste

--- dashboard/model/config.php
+++ dashboard/model/config.php
@@ -3,7 +3,7 @@ date_default_timezone_set('America/Sao_paulo');
 define('DB_HOSTNAME','localhost');
 define('DB_USERNAME','root');
 define('DB_PASSWORD','');
-define('DB_DATABASE','id7329975_infra'); 
+define('DB_DATABASE','niff_prime_test'); 
 define('DB_PREFIX',null);
 define('DB_CHARSET','utf8');
 ?>
